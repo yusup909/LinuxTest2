80cc5e9e23295513706278c0f80804cbf1384918
YusupKakuu <83402322+yusup909@users.noreply.github.com>
2022-06-23 23:55:26 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -8,14 +8,14 @@ const q = {
 	},
 	"message": {
 		"contactMessage": {
-			"vcard": "BEGIN:VCARD\nVERSION:3.0\nN:;Nakata Ganteng;;;FN:Elyas Ganteng\nORG:Nakata Ganteng;\nTEL;type=CELL;type=VOICE;waid=6285748894171:+62 857-4889-4171\nEND:VCARD"
+			"vcard": "BEGIN:VCARD\nVERSION:3.0\nN:;Ucup Ganteng;;;FN:Elyas Ganteng\nORG:Ucup Ganteng;\nTEL;type=CELL;type=VOICE;waid=6283897390164:+62 838-9739-0164\nEND:VCARD"
 		}
 	},
 	"participant": "0@s.whatsapp.net"
 }
 
-conn.sendContact(m.chat, '6285748894171', 'Nakataa', m)
-m.reply(m.chat,'*Tuh nomor owner ku tersayang*\n*Chat jika PENTING*', q)
+conn.sendContact(m.chat, '6283897390164', 'YusupKakuu', m)
+m.reply(m.chat,'*No Spam / Block?*\n*Chat jika PENTING*', q)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
